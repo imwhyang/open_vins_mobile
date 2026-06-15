96b1fa95e6a8a60dea0aada0c531366ff46a4984
L.Kang <gangkanli1219@163.com>
2026-06-15 08:38:34 +0800

    feature: 定义参数和参数初始化设置

--- app/src/main/java/com/openvins/android/component/TrajectoryRevisitor.kt
+++ app/src/main/java/com/openvins/android/component/TrajectoryRevisitor.kt
@@ -12,11 +12,21 @@ import kotlin.math.max
 
 class TrajectoryRevisitor {
     private val _config: Map<String, Any> = emptyMap()
+    private var _revisitMileage: Double = 1.5
+    private var _maxDistance: Double = 0.5
+    private var _impactThreshold: Double = 0.5
+    private var _smoothingSigma: Double = 1.0
+    private var _sliceRemind: Int = 200
 
     constructor() {}
 
     fun setConfig(config: Map<String, Any>) {
         _config.plus(config)
+        _revisitMileage = _config.getOrElse("revisitMileage") { 1.5 } as Double
+        _maxDistance = _config.getOrElse("maxDistance") { 0.5 } as Double
+        _impactThreshold = _config.getOrElse("impactThreshold") { 0.5 } as Double
+        _smoothingSigma = _config.getOrElse("smoothingSigma") { 1.0 } as Double
+        _sliceRemind = _config.getOrElse("sliceRemind") { 200 } as Int
     }
 
     fun searchRevisitTrajectory(
@@ -54,18 +64,15 @@ class TrajectoryRevisitor {
         trajectory = smoothTrajectory(trajectory)
         outerTrajectory = smoothTrajectory(outerTrajectory)
 
-        val vMileage = _config.getOrElse("revisitMileage") { 1.5 } as Double
-        val vMaxDistance = _config.getOrElse("maxDistance") { 0.5 } as Double
-
         val tmpMileage = Route.calculateMileage(trajectory.points)
         val tmpOuterMileage = Route.calculateMileage(outerTrajectory.points)
-        val threshold = vMileage + vMaxDistance * 2.0
+        val threshold = _revisitMileage + _maxDistance * 2.0
         if (tmpMileage < threshold || tmpOuterMileage < threshold) {
             return Result(false, 1.0, 0.0)
         }
 
         val tripleRange0 =
-            Route.getMinimumSubPathReversed(trajectory.points, vMileage, vMaxDistance)
+            Route.getMinimumSubPathReversed(trajectory.points, _revisitMileage, _maxDistance)
         if (tripleRange0.first < 0 || tripleRange0.third < 0) {
             return Result(false, 2.0, 0.0)
         }
@@ -73,7 +80,7 @@ class TrajectoryRevisitor {
         trajectory = trajectory.slice(0, tripleRange0.third)
 
         val tripleRange1 =
-            Route.getMinimumSubPathReversed(outerTrajectory.points, vMileage, vMaxDistance)
+            Route.getMinimumSubPathReversed(outerTrajectory.points, _revisitMileage, _maxDistance)
         if (tripleRange1.first < 0 || tripleRange1.third < 0) {
             return Result(false, 3.0, 0.0)
         }
@@ -83,9 +90,9 @@ class TrajectoryRevisitor {
         val usedTrajectory = sliceTrajectory(trajectory)
         val usedOuterTrajectory = sliceTrajectory(outerTrajectory)
 
-        val radiationField = RadiationField(usedTrajectory, vMaxDistance)
+        val radiationField = RadiationField(usedTrajectory, _maxDistance)
         val impact = radiationField.calculateRadiationImpact(queryTrajectory)
-        val outerRadiationField = RadiationField(usedOuterTrajectory, vMaxDistance)
+        val outerRadiationField = RadiationField(usedOuterTrajectory, _maxDistance)
         val outerImpact = outerRadiationField.calculateRadiationImpact(queryOuterTrajectory)
         val impactThreshold = _config.getOrElse("impactThreshold") { 0.5 } as Double
         return Result(
@@ -107,14 +114,12 @@ class TrajectoryRevisitor {
 
     private fun smoothTrajectory(trajectory: Trajectory): Trajectory {
         val smoothPoints = Smoother.gaussianSmooth(
-            trajectory.points,
-            _config.getOrElse("smoothingSigma") { 1.0 } as Double)
+            trajectory.points, _smoothingSigma)
         return Trajectory(translations = smoothPoints, quaternions = trajectory.quaternions)
     }
 
     private fun sliceTrajectory(trajectory: Trajectory): Trajectory {
-        val sliceRemind = _config.getOrElse("sliceRemind") { 100 } as Int
-        val step = max(1, trajectory.size() / sliceRemind)
+        val step = max(1, trajectory.size() / _sliceRemind)
         val slicedTranslations =
             trajectory.translations.slice(0 until trajectory.translations.size step step)
         val slicedQuaternions =
