00e690f530e9dce90d6ccb08bc0faeb1d82277cf
L.Kang <gangkanli1219@163.com>
2026-06-20 19:22:41 +0800

    fix：调整可视化参数

--- openvins-lib/src/main/java/com/openvins/android/JavaCamResView.kt
+++ openvins-lib/src/main/java/com/openvins/android/JavaCamResView.kt
@@ -12,7 +12,7 @@ class JavaCamResView(context: Context?, attrs: AttributeSet?) : JavaCameraView(c
     override fun initializeCamera(width: Int, height: Int): Boolean {
 
         // Set the max frame size
-        super.setMaxFrameSize(640, 480)
+        super.setMaxFrameSize(720, 540)
 
         // Initialize our camera
         if (!super.initializeCamera(width, height)) {
